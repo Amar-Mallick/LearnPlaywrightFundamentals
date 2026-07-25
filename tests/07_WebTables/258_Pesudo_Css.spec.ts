b74854ad03e6ead665f0d044e7471b18d72a4faa
Amar Mallick <amarmallick.sqa@gmail.com>
2026-07-25 21:14:59 +0530

    Add Pseudo CSS selector test spec for WebTable

---
+++ tests/07_WebTables/258_Pesudo_Css.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+test("Use pesudo css selector to find an element", async ({ page }) => {
+    await page.goto("https://app.thetestingacademy.com/playwright/webtable");
+    await page.pause();
+    // Uses Playwright's custom :has-text() and standard CSS :first-child
+    await page.locator("tr:has-text('Priya.Nair') td:first-child").click();
+
+    /*  We can also use this method to click checkbox but it's not pesudo concepts.
+     1. Find the row containing the text
+    const row = page.locator('tr').filter({ hasText: 'Priya.Nair' });
+    2. Click the first 'td' (column 0) in that specific row
+    await row.locator('td').nth(0).click(); */
+})
